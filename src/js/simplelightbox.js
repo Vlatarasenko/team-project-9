324b27f76346aa5b5663e7bee1b54ec8eb2286d5
Ivan Shatylo <shatilo540@gmail.com>
2022-09-04 18:32:42 +0300

    add img add js update index html +index js update galler html+scss

---
+++ src/js/simplelightbox.js
@@ -0,0 +1,8 @@
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.css";
+
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+   captionsData: 'alt',
+   captionsDelay: 250,
+});
