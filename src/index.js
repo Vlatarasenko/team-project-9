24db8c5c7f16ee6c95f90970696b63f4c11f18ae
Vlatarasenko <vlatarasenko@gmail.com>
2022-09-05 22:20:59 +0300

    fix footer

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import './js/modal-location';
 import './js/scroll-to-top';
 import './js/buy-now';
 import './js/ingredients-modal';
+import './js/ingredients-modal-second';
+import './js/ingredients-modal-third';
---
+++ src/js/ingredients-modal-third.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[ingredients-modal-open-third]"),
+    closeModalBtn: document.querySelector("[ingredients-modal-close]"),
+    modal: document.querySelector("[ingredients-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("ingredients-hidden");
+  }
+})();
