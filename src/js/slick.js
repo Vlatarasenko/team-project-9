4ada06ef0c4bebb34e8731849acebdb259d1fde8
Ivan Shatylo <shatilo540@gmail.com>
2022-09-07 00:04:14 +0300

    afdgfg

--- src/js/slick.js
+++ src/js/slick.js
@@ -9,10 +9,9 @@ $('.one-time').slick({
 
 
 $('.gallery-time').slick({
-  dots: false,
+  dots: true,
   infinite: true,
+  speed: 300,
   slidesToShow: 1,
-  adaptiveHeight: true,
-  autoplay: false,
-  autoplaySpeed: 2000,
+  adaptiveHeight: true
 });
