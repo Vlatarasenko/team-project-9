e44ca0357dcd10505dd963c0594e38f9c1877530
Vlatarasenko <107504813+Vlatarasenko@users.noreply.github.com>
2022-09-06 17:55:01 +0000

    Deploying to gh-pages from @ Vlatarasenko/team-project-9@365f395dfa4f3a1c6cbf51829288ca52aace045a 🚀

--- index.a641aa7c.js
+++
@@ -1,2 +0,0 @@
-$(".one-time").slick({dots:!0,infinite:!0,slidesToShow:1,adaptiveHeight:!0,autoplay:!0,autoplaySpeed:2e3}),$(".gallery-time").slick({dots:!0,infinite:!0,slidesToShow:1,adaptiveHeight:!0,autoplay:!1,autoplaySpeed:2e3});
-//# sourceMappingURL=index.a641aa7c.js.map
---
+++ index.ddbaa3cc.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({dots:!0,infinite:!0,slidesToShow:1,adaptiveHeight:!0,autoplay:!1,autoplaySpeed:2e3}),$(".gallery-time").slick({dots:!1,infinite:!0,slidesToShow:1,adaptiveHeight:!0,autoplay:!1,autoplaySpeed:2e3});
+//# sourceMappingURL=index.ddbaa3cc.js.map
